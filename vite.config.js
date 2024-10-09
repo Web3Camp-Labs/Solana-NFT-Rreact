602a019723007c46077f231d1189a12b20bd7dac
AAA <7518647+wendychaung@users.noreply.github.com>
2024-10-10 00:41:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import nodePolyfills from "rollup-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/solana-nft/",
+  base: "/Solana-NFT-Rreact/",
   plugins: [react()],
   resolve: {
     alias: {
